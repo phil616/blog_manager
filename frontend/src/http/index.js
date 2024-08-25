b37477eabaa08ad80cfef6c740555d0a762dd197
phil616 <Phil616@163.com>
2024-08-25 22:17:43 +0800

    remove api url

--- frontend/src/http/index.js
+++ frontend/src/http/index.js
@@ -2,9 +2,9 @@ import axios from 'axios';
 
 // 创建axios实例
 const service = axios.create({
-  baseURL: process.env.VUE_APP_API + "/api"
+  baseURL: "/api"
 });
-service.baseURL = process.env.VUE_APP_API + "/api";
+service.baseURL = "/api";
 // 请求拦截器
 service.interceptors.request.use(
   config => {
