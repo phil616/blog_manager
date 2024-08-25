9c0003a96f61ea957741dad1f327cad6562e0143
phil616 <Phil616@163.com>
2024-08-25 22:08:25 +0800

    remove baseurl for vue app

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,6 +1,6 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  publicPath: process.env.VUE_APP_BASE_URL,
+  publicPath: "/manager",
   chainWebpack: config => {
     config.plugin('html')
       .tap(args => {
